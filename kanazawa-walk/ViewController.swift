90b6614c3b2f02a0483a0c0a093e5d61ce4e4441
Fumiya Sakai <fumiya.def.mathmatica@gmail.com>
2015-10-22 02:53:33 +0900

    initial commit

---
+++ kanazawa-walk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kanazawa-walk
+//
+//  Created by 酒井文也 on 2015/10/22.
+//  Copyright © 2015年 just1factory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
